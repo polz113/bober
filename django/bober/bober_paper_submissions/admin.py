337cb5d72baf6563abc9fe5b1c5d7a525ecee8d4
polz <polz@povzpetnik>
2015-11-14 12:09:34 +0100

    Added missing admin page

---
+++ django/bober/bober_paper_submissions/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from bober_paper_submissions.models import *
+
+class JuniorAttemptInline(admin.TabularInline):
+    model = JuniorAttempt
+
+class JuniorYearAdmin(admin.ModelAdmin):
+    model = JuniorYear
+    inlines = [JuniorAttemptInline,]
+
+class JuniorYearInline(admin.TabularInline):
+    model = JuniorYear
+
+class JuniorMentorshipAdmin(admin.ModelAdmin):
+    inlines = [ JuniorYearInline ]
+
+# Register your models here.
+admin.site.register(JuniorMentorship, JuniorMentorshipAdmin)
+admin.site.register(JuniorDefaultYear)
+admin.site.register(JuniorYear, JuniorYearAdmin)
