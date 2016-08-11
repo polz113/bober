20fa1713c35297969aced92b0e720392f17aa6bd
ocelik <onurceliktas41@gmail.com>
2016-08-11 13:18:03 +0200

    tables.py for localization of list.html

--- django/bober/bober_tasks/tables.py
+++ django/bober/bober_tasks/tables.py
@@ -1,12 +1,12 @@
+from django.utils.translation import ugettext_lazy as _
 import django_tables2 as tables
 from bober_tasks.models import *
-
 class TaskTable(tables.Table):
     #title = tables.Column(verbose_name= 'Title' )
-    body = tables.Column(verbose_name= 'Description')
-    timestamp = tables.Column(verbose_name= 'Last Edit')
-    title = tables.TemplateColumn('<a href="/tasks/tasktranslation/{{record.id}}/update">{{record.title}}</a>', verbose_name= 'Title')
-    export = tables.TemplateColumn('<input type="checkbox" value="{{ record.pk }}" />', verbose_name="Export", orderable=False)
+    body = tables.Column(verbose_name=_("Description"))
+    timestamp = tables.Column(verbose_name= _('Last Edit'))
+    title = tables.TemplateColumn('<a href="/tasks/tasktranslation/{{record.id}}/update">{{record.title}}</a>', verbose_name= _('Title'))
+    export = tables.TemplateColumn('<input type="checkbox" value="{{ record.pk }}" />', verbose_name=_('Export'), orderable=False)
     class Meta:
         model = TaskTranslation
         fields = ('title', 'body', 'timestamp')
