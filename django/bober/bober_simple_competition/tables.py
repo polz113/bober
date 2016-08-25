53143a2cd5bf43e956b84fe42d79d55db3f3c67a
ocelik <onurceliktas41@gmail.com>
2016-08-25 15:09:02 +0200

    translation of list of users is done

--- django/bober/bober_simple_competition/tables.py
+++ django/bober/bober_simple_competition/tables.py
@@ -1,5 +1,6 @@
 import django_tables2 as dt2
 
+from django.utils.translation import ugettext_lazy as _
 from bober_simple_competition.models import Profile, Question
 
 class ProfileTable(dt2.Table):
@@ -7,11 +8,11 @@ class ProfileTable(dt2.Table):
         model = Profile
         fields = ('first_name', 'last_name', 'email', 'username', 'vcard')
         attrs = {'class': 'taskTable table table-hover'}
-    first_name = dt2.Column(accessor='user.first_name')
-    last_name = dt2.Column(accessor='user.last_name')
-    email = dt2.Column(accessor='user.email')
-    username = dt2.Column(accessor='user.username')
-    select = dt2.CheckBoxColumn(accessor='id')
+    first_name = dt2.Column(accessor='user.first_name', verbose_name=_("First Name"))
+    last_name = dt2.Column(accessor='user.last_name', verbose_name = _("Last Name"))
+    email = dt2.Column(accessor='user.email', verbose_name=_("E-Mail"))
+    username = dt2.Column(accessor='user.username', verbose_name=_("Username"))
+    select = dt2.CheckBoxColumn(accessor='id', verbose_name=_("vcard"))
 
 class QuestionTable(dt2.Table):
     class Meta:
