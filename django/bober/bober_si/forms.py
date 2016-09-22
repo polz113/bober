917b3ce3f7534744fb6b5d0c291f1184921cc068
polz <polz@aufbix.org>
2016-09-22 10:59:24 +0200

    Fixed the school code creation view in bober_si.

--- django/bober/bober_si/forms.py
+++ django/bober/bober_si/forms.py
@@ -11,7 +11,7 @@ class OverviewForm(forms.Form):
 
 class SchoolCodesCreateForm(forms.Form):
     school = forms.ModelChoiceField(label=_('School'), queryset=School.objects.all(),
-        widget = autocomplete.ModelSelect2(url='profile_autocomplete'))
+        widget = autocomplete.ModelSelect2(url='school_autocomplete'))
         #widget=autocomplete_light.widgets.ChoiceWidget('SchoolAutocomplete',
         #    attrs={'class':'modern-style'}))
 
