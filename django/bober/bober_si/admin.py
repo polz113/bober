9d6c00a64311bf48e7af4b0dfd90bb3257a54e7f
Trenutno prijavljeni admin na strezniku <bober@localhost>
2017-11-27 16:31:10 +0000

    Change fields in schoolteachercode admin to raw_id

--- django/bober/bober_si/admin.py
+++ django/bober/bober_si/admin.py
@@ -18,7 +18,7 @@ class SchoolTeacherCodeAdmin(DefaultAdmin):
         'teacher__user__first_name', 'teacher__user__last_name',
         'school__name'
     ]
-    raw_id_fields = ['school', 'code']
+    raw_id_fields = ['school', 'code', 'teacher']
 
 
 class SchoolAdmin(DefaultAdmin):
