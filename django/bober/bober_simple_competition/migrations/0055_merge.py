b3db2bc1252309ba584c4ccd27c8d6a56b3b8db3
Trenutno prijavljeni admin na strezniku <bober@localhost>
2018-02-20 05:36:43 +0000

    Fix broken migrations, start fixing teacher recognition template

--- django/bober/bober_simple_competition/migrations/0055_merge.py
+++ django/bober/bober_simple_competition/migrations/0055_merge.py
@@ -8,7 +8,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('bober_simple_competition', '0054_auto_20170129_1434'),
-        ('bober_simple_competition', '0053_auto_20161111_2219'),
+        ('bober_simple_competition', '0053_profile_date_of_birth'),
     ]
 
     operations = [
