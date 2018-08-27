bb479710e8aef5847f11bc54c38776cb672f3eb9
gregor <gregor@midime.jerse.info>
2018-08-27 08:54:40 +0200

    PEP8

--- django/bober/bober_paper_submissions/admin.py
+++ django/bober/bober_paper_submissions/admin.py
@@ -1,19 +1,25 @@
 from django.contrib import admin
-from bober_paper_submissions.models import *
+from bober_paper_submissions.models import JuniorAttempt, JuniorYear, JuniorMentorship,\
+    JuniorDefaultYear
+
 
 class JuniorAttemptInline(admin.TabularInline):
     model = JuniorAttempt
 
+
 class JuniorYearAdmin(admin.ModelAdmin):
     model = JuniorYear
-    inlines = [JuniorAttemptInline,]
+    inlines = [JuniorAttemptInline]
+
 
 class JuniorYearInline(admin.TabularInline):
     model = JuniorYear
 
+
 class JuniorMentorshipAdmin(admin.ModelAdmin):
-    inlines = [ JuniorYearInline ]
+    inlines = [JuniorYearInline]
     search_fields = ['teacher__user__username', 'school__name']
+
 
 # Register your models here.
 admin.site.register(JuniorMentorship, JuniorMentorshipAdmin)
