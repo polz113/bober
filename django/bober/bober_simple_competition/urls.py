c87ca0523f6a32c09a8691ec5f3d14c060472d26
gregor <gregor@midime.jerse.info>
2018-08-27 12:21:46 +0200

    PEP8

--- django/bober/bober_simple_competition/urls.py
+++ django/bober/bober_simple_competition/urls.py
@@ -29,8 +29,10 @@ urlpatterns = [
     # 1. login / enter access code
     url(r'^access_code/*(?P<next>.*)$', views.access_code, name="access_code"),
     # 2. pick competition
-    url(r'^competition/id=$', views.CompetitionList.as_view(), name="competition_list"),
-    url(r'^competition/$', views.CompetitionList.as_view(), name="competition_list"),
+    url(r'^competition/id=$', views.CompetitionList.as_view(),
+        name="competition_list"),
+    url(r'^competition/$', views.CompetitionList.as_view(),
+        name="competition_list"),
     url(r'^competition/id=(?P<pk>[\w\-_]+)/detail$',
         views.CompetitionDetail.as_view(), name="competition_detail"),
     url(r'^competition/(?P<slug>[\w\-_]+)/detail$',
@@ -72,17 +74,18 @@ urlpatterns = [
     url(r'^competition/(?P<slug>[\w\-_]+)/attempts/$',
         views.competition_attempt_list, name="competition_attempt_list"),
     url(r'^competition/id=(?P<pk>\d+)/attempts/regrade$',
-        views.competition_attempt_list, {'regrade':True}, name="competition_attempt_list"),
+        views.competition_attempt_list,
+        {'regrade': True}, name="competition_attempt_list"),
     url(r'^competition/(?P<slug>[\w\-_]+)/attempts/regrade$',
-        views.competition_attempt_list, {'regrade':True}, name="competition_attempt_list"),
+        views.competition_attempt_list,
+        {'regrade': True}, name="competition_attempt_list"),
     #     2.1.4 mark attempts as invalid
     #           all attempts with codes created or distributed by
     #           the current user can be accessed
     url(r'^competition/(?P<slug>[\w\-_]+)/attempts/(?P<competition_questonset_id>\d+)/(?P<attempt_id>\d+)/invalidate$',
-        views.competition_attempt_list, {'regrade':True}, name="attempt_invalidate"),
+        views.competition_attempt_list,
+        {'regrade': True}, name="attempt_invalidate"),
     #     2.1.5
-    #url(r'^competition/(?P<slug>[\w\-_]+)/questionsets/use$',
-    #    views.use_questionsets, name="use_questionsets"),
     url(r'^competition/(?P<slug>[\w\-_]+)/questionsets/use$',
         views.use_questionsets, name="use_questionsets"),
     url(r'^competition/(?P<slug>[\w\-_]+)/questionsets/(?P<competition_questionset_id>[\d]+)$',
@@ -91,7 +94,7 @@ urlpatterns = [
     #     2.2.0 register as competitor using a code
     # compatibility redirect
     url(r'^competitions/(?P<slug>[\w\-_]+)/$',
-        RedirectView.as_view(pattern_name='competition_compete'), 
+        RedirectView.as_view(pattern_name='competition_compete'),
         name="competition_compete_compat"),
     url(r'^competition/(?P<slug>[\w\-_]+)/$',
         views.CompetitionCompete.as_view(), name="competition_compete"),
@@ -108,13 +111,13 @@ urlpatterns = [
     #     2.2.1 get question page
     url(r'^compete/(?P<competition_questionset_id>[\d]+)/resources/competition.html$',
         views.competition_index, name="competition_index"),
-	#     2.2.1.1 get question page as guest :: GUEST
+    # 2.2.1.1 get question page as guest :: GUEST
     url(r'^guest/(?P<competition_questionset_id>[\d]+)/$',
         views.competition_guest, name="competition_guest"),
     #     2.2.2 get question resources
     url(r'^compete/(?P<competition_questionset_id>\d+)/resources/(?P<resource_path>.*)',
         views.competition_resources, name="competition_resources"),
-    #     2.2.3 get question data (existing answers, attempt_id, randomised_question map)
+    # 2.2.3 get question data (existing answers, attempt_id, randomised_question map)
     url(r'^compete/(?P<competition_questionset_id>\d+)/data.json$',
         views.competition_data, name="competition_data"),
     #     2.2.4 get remaining time
@@ -130,9 +133,11 @@ urlpatterns = [
         views.finish_competition, name="finish_competition"),
     #     2.2.7 view results
     url(r'^compete/(?P<competition_questionset_id>\d+)/attempts/regrade$',
-        views.competition_attempt_list, {'regrade':True}, name="competition_attempt_list_regrade"),
+        views.competition_attempt_list, {'regrade': True},
+        name="competition_attempt_list_regrade"),
     url(r'^compete/(?P<competition_questionset_id>\d+)/attempts/$',
-        views.competition_attempt_list, {'regrade':False}, name="competition_attempt_list"),
+        views.competition_attempt_list, {'regrade': False},
+        name="competition_attempt_list"),
     url(r'^compete/(?P<competition_questionset_id>\d+)/attempts/(?P<attempt_id>\d+)/$',
         views.attempt_results, name="attempt_results"),
     url(r'^compete/(?P<competition_questionset_id>\d+)/attempts/(?P<attempt_id>\d+)/confirm$',
@@ -140,52 +145,65 @@ urlpatterns = [
     url(r'^compete/(?P<competition_questionset_id>\d+)/attempts/(?P<attempt_id>\d+)/unconfirm$',
         views.attempt_unconfirm, name="attempt_unconfirm"),
     # 3. create registration codes
-    url(r'^registration_codes/$', views.registration_codes, name="registration_codes"),
+    url(r'^registration_codes/$', views.registration_codes,
+        name="registration_codes"),
     # 5. edit user data
     # url(r'^users/$', views.ProfileListView.as_view(), name="profile_list"),
-    url(r'^competitor/(?P<pk>\d+)/update', views.CompetitorUpdateJson.as_view(), name='competitor_update'),
+    url(r'^competitor/(?P<pk>\d+)/update',
+        views.CompetitorUpdateJson.as_view(), name='competitor_update'),
     url(r'^profile/$', views.ProfileTableView.as_view(), name="profile_list"),
-    url(r'^profile/(?P<pk>\d+)/$', views.ProfileDetail.as_view(), name="profile_detail"),
+    url(r'^profile/(?P<pk>\d+)/$', views.ProfileDetail.as_view(),
+        name="profile_detail"),
     #   5.1 merge users
     #    any users registered with codes created or distributed
     #    by the current user can be merged
     #   5.2 edit users
     #    the data for users registered with codes created or distributed
     #    by the current user can be edited
-    url(r'^profile/(?P<pk>\d+)/update$', views.ProfileUpdate.as_view(), name="profile_update"),
-    url(r'^profile/(?P<pk>\d+)/merge$', views.ProfileMerge.as_view(), name="profile_merge"),
+    url(r'^profile/(?P<pk>\d+)/update$', views.ProfileUpdate.as_view(),
+        name="profile_update"),
+    url(r'^profile/(?P<pk>\d+)/merge$', views.ProfileMerge.as_view(),
+        name="profile_merge"),
     #   5.3 get certificates, other files
-    url(r'^profile/(?P<pk>\d+)/files/(?P<resource_path>)$', views.profile_files, name="profile_files"),
+    url(r'^profile/(?P<pk>\d+)/files/(?P<resource_path>)$',
+        views.profile_files, name="profile_files"),
     url(r'^profile/send-email$', views.send_email, name="send_email"),
     url(r'^send_to_mail$', views.send_to_mail, name="send_to_mail"),
     # 6. import question(s)
     url(r'^question/$', views.QuestionList.as_view(), name="question_list"),
-    # TODO: figure out a way to have safe checkers
-    #url(r'^question/import$', views.QuestionImport.as_view(), name="question_import"),
-    #url(r'^question/create$', views.QuestionCreate.as_view(), name="question_create"),
-    #url(r'^question/(?P<pk>\d+)/update$', views.QuestionUpdate.as_view(), name="question_update"),
-    url(r'^question/(?P<pk>\d+)/$', views.QuestionDetail.as_view(), name="question_detail"),
-    url(r'^question/(?P<pk>\d+)/$', views.QuestionDetail.as_view(), name="question_index"),
-    # url(r'^question/(?P<pk>\d+)/resources/$', views.QuestionDetail.as_view(), name="question_text"),
-    url(r'^question/(?P<pk>\d+)/resources/(?P<resource_path>.*)$', views.question_resources, name="question_resource"),
-    url(r'^question/(?P<pk>\d+)/solution/$', views.QuestionSolution.as_view(), name="question_solution"),
-    url(r'^question/(?P<pk>\d+)/solution/(?P<resource_path>.*)$', views.question_resources, name="question_solution_resource"),
+    url(r'^question/(?P<pk>\d+)/$', views.QuestionDetail.as_view(),
+        name="question_detail"),
+    url(r'^question/(?P<pk>\d+)/$', views.QuestionDetail.as_view(),
+        name="question_index"),
+    url(r'^question/(?P<pk>\d+)/resources/(?P<resource_path>.*)$',
+        views.question_resources, name="question_resource"),
+    url(r'^question/(?P<pk>\d+)/solution/$', views.QuestionSolution.as_view(),
+        name="question_solution"),
+    url(r'^question/(?P<pk>\d+)/solution/(?P<resource_path>.*)$',
+        views.question_resources, name="question_solution_resource"),
     # 7. create questionset from questions
-    url(r'^questionset/id=$', views.QuestionSetList.as_view(), name="questionset_list"),
-    url(r'^questionset/$', views.QuestionSetList.as_view(), name="questionset_list"),
+    url(r'^questionset/id=$', views.QuestionSetList.as_view(),
+        name="questionset_list"),
+    url(r'^questionset/$', views.QuestionSetList.as_view(),
+        name="questionset_list"),
     url(r'^questionset/id=create', views.QuestionSetCreate.as_view(),
         name="questionset_add"),
     url(r'^questionset/id=create', views.QuestionSetCreate.as_view(),
         name="questionset_create"),
-    url(r'^questionset/$', views.QuestionSetList.as_view(), name="questionset_id_list"),
-    url(r'^questionset/(?P<slug>[\w\-_]+)/update$', views.QuestionSetUpdate.as_view(),
+    url(r'^questionset/$', views.QuestionSetList.as_view(),
+        name="questionset_id_list"),
+    url(r'^questionset/(?P<slug>[\w\-_]+)/update$',
+        views.QuestionSetUpdate.as_view(),
         name="questionset_update"),
-    url(r'^questionset/id=(?P<pk>\d+)/update$', views.QuestionSetUpdate.as_view(),
+    url(r'^questionset/id=(?P<pk>\d+)/update$',
+        views.QuestionSetUpdate.as_view(),
         name="questionset_change"),
     url(r'^questionset/id=(.*)/update$', views.QuestionSetUpdate.as_view(),
         name="questionset_change"),
-    url(r'^questionset/id=(?P<pk>\d+)/delete$', views.QuestionSetDelete.as_view()),
-    url(r'^questionset/(?P<slug>[\w\-_]+)/delete$', views.QuestionSetDelete.as_view()),
+    url(r'^questionset/id=(?P<pk>\d+)/delete$',
+        views.QuestionSetDelete.as_view()),
+    url(r'^questionset/(?P<slug>[\w\-_]+)/delete$',
+        views.QuestionSetDelete.as_view()),
     url(r'^questionset/id=(?P<pk>\d+)/$', views.QuestionSetDetail.as_view(),
         name="questionset_detail"),
     url(r'^questionset/(?P<slug>[\w\-_]+)/$', views.QuestionSetDetail.as_view(),
@@ -204,11 +222,13 @@ urlpatterns = [
         name="competitor_code_format_list"),
     url(r'^code_format/admin/(?P<pk>\d+)$', views.CodeFormatDetail.as_view(),
         name="admin_code_detail"),
-    url(r'^code_format/competitor/(?P<pk>\d+)$', views.CodeFormatDetail.as_view(),
+    url(r'^code_format/competitor/(?P<pk>\d+)$',
+        views.CodeFormatDetail.as_view(),
         name="competitor_code_detail"),
     url(r'^code_format/admin/create$', views.AdminCodeFormatCreate.as_view(),
         name="admin_code_format_create"),
-    url(r'^code_format/competitor/create$', views.CompetitorCodeFormatCreate.as_view(),
+    url(r'^code_format/competitor/create$',
+        views.CompetitorCodeFormatCreate.as_view(),
         name="competitor_code_format_create"),
     # shortcut for registering and competing immediately
 ]
