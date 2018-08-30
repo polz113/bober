cf3465dc60517ff6533841635a1d3e5eb5ca104a
gregor <gregor@midime.jerse.info>
2018-08-30 14:23:50 +0200

    Make Middleware compatible with Django 2.1

--- django/bober/bober_simple_competition/middleware.py
+++ django/bober/bober_simple_competition/middleware.py
@@ -1,13 +1,19 @@
-class ProfileMiddleware(object):
+class ProfileMiddleware:
     """
     Add profile to autenticated user requests.
     It is available as profile property on request object.
     """
-    def process_request(self, request):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         if not hasattr(request, 'profile') and \
                 request.user.is_authenticated():
             profile = request.user.profile
             if profile.merged_with is not None:
                 profile = profile.merge_to_top(limit=5)
             request.profile = profile
-        return None
+
+        response = self.get_response(request)
+        return response
