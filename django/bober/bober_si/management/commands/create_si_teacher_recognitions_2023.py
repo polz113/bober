aad90d2497112f8d616c9c450f555738b1aabdfd
polz <polz@polz.si>
2026-03-15 16:39:23 +0100

    Fix names for award_listing, next_round_listing

--- django/bober/bober_si/management/commands/create_si_teacher_recognitions_2023.py
+++ django/bober/bober_si/management/commands/create_si_teacher_recognitions_2023.py
@@ -499,7 +499,7 @@ def _compose_text(competition, teacher, attempts, template):
     by_groups = []
     n_txt = _n_texts(attempts)
     if n_txt is not None:
-        n_confirmed, next_round, award, top_places = n_txt
+        n_confirmed, next_round_listing, award_listing, top_places = n_txt
     for cqs in competition.competitionquestionset_set.all():
         cqs_name = cqs.name
         c_attempts = attempts.filter(competitionquestionset = cqs)
